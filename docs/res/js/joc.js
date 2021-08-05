6e032f5dc993fb2cf7d85fe1f94570c7913a1537
slevi123 <simofilevente@gmail.com>
2021-08-05 22:00:10 +0300

    half functioning game

--- docs/res/js/joc.js
+++ docs/res/js/joc.js
@@ -3,6 +3,38 @@ sentimente = [{"nev": "boldogság", "nume": "fericire"}, {"nev": "vidámság", "
 morale = [{"nev": "jó", "nume": "bun"}, {"nev": "rossz", "nume": "rău"}, {"nev": "okos", "nume": "deștept"}, {"nev": "szerető", "nume": "iubitor"}, {"nev": "tapintatos", "nume": "amabil"}, {"nev": "idétlen", "nume": "urâcios"}]
 fizice = [{"nev": "magas", "nume": "înalt"}, {"nev": "gyenge", "nume": "slab"}, {"nev": "rövid", "nume": "scund"}, {"nev": "kövér", "nume": "gras"}, {"nev": "szőke", "nume": "blond"}, {"nev": "göndör", "nume": "creț"}, {"nev": "barna", "nume": "saten"}]
 
+
+class GameOptions {
+    constructor(){
+        this.answer_count = 4;
+        this.answer_id = undefined;
+        this._punctaj = 0;
+
+        this.__punctaj_dom__ = undefined;
+    }
+
+    get punctaj(){
+        return this._punctaj;
+    }
+
+    set punctaj(new_value){
+        this._punctaj = new_value;
+        if (this.__punctaj_dom__) this.__punctaj_dom__.textContent = "punctaj: " + this._punctaj;
+    }
+
+    bind(){
+        this.__punctaj_dom__ = document.getElementById("punctaj");
+
+        let answer_num_dom = document.getElementById("answer-num-option");
+        this.answer_count = answer_num_dom.value;
+        answer_num_dom.addEventListener("change", ()=> {
+            this.answer_count = answer_num_dom.value;
+        })
+    }
+}
+
+let game_options = new GameOptions()
+
 function ranlist(list){
     return list[~~(Math.random() * list.length)];
 }
@@ -14,8 +46,22 @@ function remove_children(dom_element){
         child = dom_element.lastElementChild;
     }
 }
+function setTime(timer_dom, timp){
+    let minutes = ~~(timp/60)
+    if (minutes){
+        let seconds = timp%60;
+        let hours = ~~(minutes/60);
+        if (hours){
+            minutes = minutes%60;
+            timer_dom.textContent="timpul: "+ hours + " h, " +minutes + " min, " + seconds +" sec";
+            // TODO: gameover after an hour
+        } else timer_dom.textContent="timpul: "+ minutes + " min, " + seconds +" sec";
+    } else timer_dom.textContent="timpul: "+ timp + " sec";
+}
 
 function on_start(){
+    game_options.bind();
+
     document.getElementById("start-button").style.display = "none";
     document.getElementById("game").style.display = "block";
     let timer_dom = document.getElementById("timer")
@@ -23,24 +69,23 @@ function on_start(){
     timer_dom.textContent="timpul: 0";
     setInterval(()=>{
         timp += 1;
-        let minutes = ~~(timp/60)
-        if (minutes){
-            let seconds = timp%60;
-            let hours = ~~(minutes/60);
-            if (hours){
-                minutes = minutes%60;
-                timer_dom.textContent="timpul: "+ hours + " h, " +minutes + " min, " + seconds +" sec";
-                // TODO: gameover after an hour
-            } else timer_dom.textContent="timpul: "+ minutes + " min, " + seconds +" sec";
-        } else timer_dom.textContent="timpul: "+ timp + " sec";
+        setTime(timer_dom, timp);
     }, 1000);
     new_word();
+}
+
+function evaluate_solution(answer_count){
+    if (game_options.answer_id==answer_count){
+        game_options.punctaj +=1;
+    }
+    new_word()
 }
 
 function add_answer_child(parent, content, answer_count){
     let new_dom = document.createElement("p");
     new_dom.textContent = content;
-    new_dom.setAttribute("answer-count", answer_count);
+    // new_dom.setAttribute("answer-count", answer_count);
+    new_dom.onclick=()=>evaluate_solution(answer_count);
     parent.appendChild(new_dom);
 }
 
@@ -49,14 +94,13 @@ function new_word(){
     question_dom = document.getElementById("question")
     answers_dom = document.getElementById("answer-options")
 
-    let answer_count = 4;
-
     let question = ranlist(teme);
-    let answer_id = ~~(Math.random * (answer_count))
+    game_options.answer_id = ~~(Math.random() * game_options.answer_count)
+    // let answer_id = 3
     let answers = [];
     let temp_option = undefined;
-    for (let i=0; i<answer_count; i++){
-        if (i==answer_id){
+    for (let i=0; i<game_options.answer_count; i++){
+        if (i==game_options.answer_id){
             answers.push(question);
         } else{
             do{
@@ -69,7 +113,7 @@ function new_word(){
     question_dom.textContent = question.nev;
 
     remove_children(answers_dom)
-    for (let i=0; i<answer_count; i++){
+    for (let i=0; i<game_options.answer_count; i++){
         add_answer_child(answers_dom, answers[i].nume, i);
     }
 }
